ad288ea810ccb8963d2c74141f558d2e138249d7
pearlsutton <112727927+pearlsutton@users.noreply.github.com>
2022-09-05 22:30:40 -0400

    Lab 1: adding a a testing page

---
+++ testing.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>CS 008 Pearl Website</title>
+        <meta name="author" content="Pearl Sutton">
+        <meta name="description" content="Sample test page to play around.">
+        <style>
+        </style>
+    </head>
+
+    <body>
+
+        <h1>Testing, for the next six weeks we will be conducting a test.</h1>
+        
+        <p>Checking to see if works.</p>
+    </body>
+</html>
