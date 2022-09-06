2a390c380dd2aba594baaea7927781c6543ddffa
pearlsutton <112727927+pearlsutton@users.noreply.github.com>
2022-09-05 22:21:59 -0400

    Lab 1: initial setup

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>CS 008 Pearl Website</title>
+        <meta name="author" content="Pearl Sutton">
+        <meta name="description" content="This is a site map for my CS 008 course here at UVM.">
+        <style>
+            figure{
+                float: left; 
+                text-align: center; 
+                width: 20%;    
+            }
+            
+            img{
+                max-width: 100%;
+            }
+        </style>
+    </head>
+
+    <body>
+        <figure>
+            <img alt="Pearl Sutton UVM Fall 2022" src="images/pearl-sutton.png">
+            <figcaption>My first semester at UVM.</figcaption>
+        </figure>
+
+        <h1>CS 008-<em>OL1</em> Fall 2022</h1>
+        <h2>Pearl Sutton - Site map</h2>
+        <p><a href="ADMIN/admin.php">My Admin Folder</a></p>
+
+        <h2>Lab one - Setting up your sitemap.</h2>
+        <p><a href="testing.php">Testing Page</a></p>
+    </body>
+</html>
