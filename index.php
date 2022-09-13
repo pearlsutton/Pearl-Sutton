f548ab069a15429e5c38d6c659a1c12448f018a2
pearlsutton <112727927+pearlsutton@users.noreply.github.com>
2022-09-13 01:55:35 -0400

    Lab 2: created home page

--- index.php
+++ index.php
@@ -2,11 +2,15 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>CS 008 Pearl Website</title>
+        <title>Ways to Help Others</title>
         <meta name="author" content="Pearl Sutton">
-        <meta name="description" content="This is a site map for my CS 008 course here at UVM.">
+        <meta name="description" content="One of the ways we can all make our world a better place is to help one another. Luckily for us, there are many ways we can do this. Here we present a variety of opportunities and actions people can begin to help those around them and around the world.">
+
+        <!-- Included Volunteering  time, donating to important causes. Perhaps mabye postive impacts parapgraph/section -->
+
         <style>
             figure{
+
                 float: left; 
                 text-align: center; 
                 width: 20%;    
@@ -19,49 +23,73 @@
     </head>
 
     <body>
-        <figure>
-            <img alt="Pearl Sutton UVM Fall 2022" src="images/pearl-sutton.png">
-            <figcaption>My first semester at UVM.</figcaption>
-        </figure>
-
-        <h1>CS 008-<em>OL1</em> Fall 2022</h1>
-        <h2>Pearl Sutton - Site map</h2>
-        <p><a href="ADMIN/admin.php">My Admin Folder</a></p>
-
-        <section class="grid-layout">
-            <h2 class="header">Lab Two - Choosing your topic.</h2>
-            <section class="public-files">
-                <h3>Public Files</h3>
-                <p><a href="lab2/index.php">index.php</a></p>      
-            </section>
-
-            <section class="supporting-files">
-                <h3>Supporting files</h3>
-            </section>
-
-            <section class="grader-notes">
-                <h3>Notes to grader</h3>
-                <p></p>
-            </section>
-        </section>
-
-        <section class="grid-layout">
-            <h2>Lab one - Setting up your sitemap.</h2>
-            <section class="public-files">
-                <h3>Public Files</h3>
-                <p><a href="test-page.php">Testing Page</a></p>      
-            </section>
-            <section class="supporting-files">
-                <h3>Supporting files</h3>
-                <p></p>
-            </section>
-
-            <section class="grader-notes">
-                <h3>Notes to grader</h3>
-                <p></p>
-            </section>
-        </section>
-
+        <main>
+            <h1>Ways to Help Others</h1>
         
+            <section>
+                <h2>Being Kind</h2>
+                <p>One of the first ways that people can begin helping others that does not involve much energy to perfrom is to be kind to one another.</p>
+                <p>Being kind and respectful to one another can work to spread positivity and understanding to those around us.</p>
+        <!-- perhaps reword these paragraphs a bit more -->
+
+            </section>
+
+            <section>
+                <h2>Volunteering</h2>
+                <p>Volunteering your time to help at shelters and important causes is a great and direct way of reaching out to help those that might need help.</p>
+
+                <p>Some of the places that you can volunteer at are:</p>
+                <table>
+                    <tr>
+                        <th>Organizations</th>    <th>Shelters</th>
+                        <th>Local Centers</th>
+                    </tr>
+                    
+                    <tr>
+                        <td>Disaster Relief</td>
+                        <td>Homeless</td>
+                        <td>Parks & Recreation</td>
+                    </tr>
+
+                    <tr>
+                        <td>Human & Civil Rights</td>
+                        <td>Animal</td>
+                        <td>Public Library</td>
+                    </tr>
+
+                    <tr>
+                        <td>Political & Advocacy Groups</td>
+                        <td>Soup Kitchens</td>
+                        <td>Community Center</td>
+                    </tr>
+
+                    <tr>
+                        <td colspan="3">Source: <cite><a href="https://ofhsoupkitchen.org/100-ways-to-help-others" target="_blank">https://ofhsoupkitchen.org/100-ways-to-help-others<a></cite></td>
+                    </tr>
+
+                    <tr>
+                    <td colspan="3">Source: <cite><a href="https://www.moneycrashers.com/good-places-volunteer-opportunities-organizations/" target="_blank">https://www.moneycrashers.com/good-places-volunteer-opportunities-organizations/<a></cite></td>
+
+                </table>
+
+            </section>
+
+            <section>
+                <h2>Donating</h2>
+                <p>Another way to help others is to donate. Donating money is not the only way for one to do this. Donating old or not-used anymore items is a great way to give to others. These could include:</p>
+                <ul>
+                    <li>Clothes</li>
+                    <li>Books</li>
+                    <li>Toys</li>
+                    <li>Source: <cite>Pearl Sutton</cite></li>
+
+                </ul>
+                <p>Donating fresh or canned food is another way for people to donate.</p>
+                <figure style="float: left; text-align: center; width 20%;">
+                    <img src="https://www.thespruceeats.com/thmb/XQPDM0pa4_L7bVcxO8JJ5xR4QQE=/2121x1414/filters:fill(auto,1)/cannedfoodsWarren_Price-2d07bf3a98814f7b8f061e800a509627.jpg" alt="Canned Food">
+                <figcaption><cite><a href="https://www.thespruceeats.com/how-long-does-canned-food-really-last-5114518" target="_blank">Vt Fish and Wildlife</a></cite></figcaption>
+                </figure>
+            </section> 
+        </main>
     </body>
-</html>
+    </html>
